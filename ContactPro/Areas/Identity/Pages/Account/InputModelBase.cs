594a057f551ee01b069314303b4691d8cc1edffd
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-10-29 22:57:51 -0400

    Add migration files and related scafolded items

---
+++ ContactPro/Areas/Identity/Pages/Account/InputModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace ContactPro.Areas.Identity.Pages.Account
+{
+    public class InputModelBase
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2) and at max {1} characters long", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2) and at max {1} characters long", MinimumLength = 2)]
+        public string? LastName { get; set; }
+    }
+}
