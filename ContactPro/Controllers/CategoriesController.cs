0bf82c800e629cd81e666a018c3e586bf3c98177
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-11-06 21:13:02 -0500

    Add using statements. Add public virtual IColllection for Contacts and Categories.

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using ContactPro.Data;
 using ContactPro.Models;
 
